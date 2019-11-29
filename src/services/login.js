5604d66e34ff0872901e5c5cd84f154238a779f0
Leandro Costa <leandroacosta2008@gmail.com>
2019-11-29 15:38:26 -0300

    login auth credentials

--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+// Fake Login
+// "email": "eve.holt@reqres.in"
+// "password": "cityslicka"
+
 export default ({ email, password }) =>
   axios.post('https://reqres.in/api/login', {
     email,
